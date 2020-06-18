f3775e12df4478f1b5069776f0893bcdb61540ab
Anthony Clemento <anthony.clemento@swc-clemento-lt.local>
2020-06-17 19:33:38 -0700

    Begin adaptive-vfc read function

---
+++ R/read_adaptive_vcf.R
@@ -0,0 +1,6 @@
+#' Read microhap, run-timing, omy05 inversion, or sexID VCF file
+#'
+#' blah blah blah long descrip here
+#' @param datapath path to selected adaptive-panel vcf files. If a directory, then all vcf files within it are
+#' processed. If a single file, with a vcf extension then only that file will be loaded.
+#' @export
