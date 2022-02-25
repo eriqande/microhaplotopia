5c5b77410725d2c14e256f753ba7e19208666792
EllenMCampbell <ellen.campbell@noaa.gov>
2022-02-24 16:58:01 -0800

    Edited filter_raw_microhap data, so it just does a light filter (just filters on haplotype depth, total depth, and allele balance--does not do anything to NX alleles or extra alleles).  Added grouping by group in addition to indiv.ID and locus to distinguish reruns of the same extraction.  Does not add rows for homozygotes--not sure if I should add that here or as a separate function, so leaving it out for now but may change later

--- R/filter_raw_microhap_data.R
+++ R/filter_raw_microhap_data.R
@@ -1,65 +1,30 @@
 #' Filter raw microhaplot output from read_unfiltered_observed().
 #'
 #' Given a set of filtering criteria on individual haplotype depth, total read depth for
-#' the genotype and allelle balance (the ratio of read 2 depth to read 1 depth) this
-#' function will filter raw amplicon sequencing data. The input file here could be
-#' what was loaded into R using the read_unfiltered_observed() function from this package.
+#' the genotype and allelle balance (the ratio of a particular microhaplotype read depth
+#' to read depth of the most common microhaplotype for an individual at a locus) this
+#' function will filter raw amplicon sequencing data. The input file here could be what
+#' was loaded into R using the read_unfiltered_observed() function from this package.
 #' @param hap_raw dataframe of unfiltered haplotypes. This could be from a single run or a
 #'  large number of runs that have been joined together.
 #' @param haplotype_depth remove haplotypes with less than depth specified here. This is for removing what's
 #'  considered potential genotyping errors or depths you don't feel confident about.
 #' @param total_depth minimum number of reads required for the genotype to be retained.
-#' @param allele_balance For heterozygotes only. The ratio of the second haplotype read depth to the first
-#'  haplotype read depth. For example, genotype with read depth of 30 and 20, the allele balance is 0.66
-#' @param retain_x_haps Argument to specify if the user wants to retain haplotypes with 'X' in them.
-#' We've determined the 'X' is likely an indel. The default is for all haplotypes with 'X' in them
-#' to be removed. If you want to retain (not filter out) haplotypes with 'X' in them set this argument
-#' to TRUE
+#' @param allele_balance Ratio of the number of reads for a given microhaplotype divided
+#' by the number of reads of the most common microhaplotype for that individual at that
+#' locus.  For example, if you have a heterozygote with 60 reads for the haplotype AA and
+#' 100 reads for the haplotype TC, the allele balance for the haplotype AA for that individual
+#' at that locus is 0.6 and is 1.0 for TC.
 #' @export
 filter_raw_microhap_data <- function(hap_raw,
                                      haplotype_depth,
                                      total_depth,
-                                     allele_balance,
-                                     retain_x_haps = FALSE) {
-
-  if (retain_x_haps == FALSE) {
-
-    hap_fil1 <- hap_raw %>%
-      filter(!str_detect(haplo, "N|X"),
-             depth >= haplotype_depth) %>%
-      arrange(indiv.ID, locus, desc(depth)) %>%
-      group_by(indiv.ID, locus) %>%
-      mutate(rank = row_number(),
-             allele.balance = depth / depth[1]) %>%
-      filter(allele.balance >= allele_balance) %>%
-      mutate(gt_type = ifelse(n() > 1, "het", "hom"),
-             depth_total = ifelse(gt_type == "het", sum(depth[1], depth[2]), depth)) %>%
-      ungroup() %>%
-      filter(depth_total >= total_depth)
-  } else {
-    hap_fil1 <- hap_raw %>%
-      filter(!str_detect(haplo, "N"),
-             depth >= haplotype_depth) %>%
-      arrange(indiv.ID, locus, desc(depth)) %>%
-      group_by(indiv.ID, locus) %>%
-      mutate(rank = row_number(),
-             allele.balance = depth / depth[1]) %>%
-      filter(allele.balance >= allele_balance) %>%
-      mutate(gt_type = ifelse(n() > 1, "het", "hom"),
-             depth_total = ifelse(gt_type == "het", sum(depth[1], depth[2]), depth)) %>%
-      ungroup() %>%
-      filter(depth_total >= total_depth)
-  }
-
-  homozygotes2add <- hap_fil1 %>% filter(gt_type == "hom") %>% mutate(rank = 2)
-
-  long_genos <- bind_rows(hap_fil1, homozygotes2add) %>%
-    group_by(source, indiv.ID, locus) %>%
-    mutate(n_rows = n()) %>%
+                                     allele_balance) {
+  hap_raw %>%
+    filter(allele.balance >= allele_balance) %>% #Allele balance filter
+    filter(depth > haplotype_depth) %>% #individual allele depth filter
+    group_by(group, indiv.ID, locus) %>%
+    mutate(TotalDepth = sum(depth), NAlleles = n()) %>% #Add columns for total depth and number of alleles per locus per individual
     ungroup() %>%
-    filter(n_rows == 2) %>%
-    dplyr::select(-n_rows) %>%
-    arrange(indiv.ID, locus)
-
-  long_genos
+    filter(TotalDepth >= total_depth)
 }
